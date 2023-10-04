22a9c1c44ac1df464a335acda5403622f3b173b3
Marney Pratt <mcpratt@smith.edu>
2023-10-04 09:06:07 -0400

    edited the comments to be clearer

--- script_templates/descriptive_stats.R
+++ script_templates/descriptive_stats.R
@@ -1,11 +1,11 @@
 
 # the code below will calculate descriptive statistics for a variable of interest grouped by another variable
 
-___ |>                            #put the name of the data frame here
-  filter(!is.na(___)) |>          #put the name of the continuous variable of interest here
-  group_by(___) |>                #put the name of the grouping variable (=factor) here
-  summarize(mean = mean(___),     #put the name of the continuous variable of interest here
-            median = median(___), #put the name of the continuous variable of interest here
-            SD = sd(___),         #put the name of the continuous variable of interest here
-            IQR = IQR(___),       #put the name of the continuous variable of interest here
+___ |>                            #name of the data frame here
+  filter(!is.na(___)) |>          #name of the continuous variable of interest here
+  group_by(___) |>                #name of the grouping variable (=factor) here
+  summarize(mean = mean(___),     #name of the continuous variable of interest here
+            median = median(___), #name of the continuous variable of interest here
+            SD = sd(___),         #name of the continuous variable of interest here
+            IQR = IQR(___),       #name of the continuous variable of interest here
             n())                  #don't put anything here, leave as is
