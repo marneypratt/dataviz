c9868bb144c597718e08c62b7fdfdf8071d681c7
Marney Pratt <mcpratt@smith.edu>
2025-02-18 16:22:35 -0500

    updated the bootstrap 95% CI to use the bias-corrected and accelerated method and increased the number of bootstrapped samples to 5000

--- script_templates/inference/cohens_d_unequal.variance.R
+++ script_templates/inference/cohens_d_unequal.variance.R
@@ -2,7 +2,7 @@
 #you need the rstatix package loaded for this code to work
 
 
-#standardized effect size for normal data
+#standardized effect size for normal data but unequal variance
 rstatix::cohens_d(
   data = ___,         #put the dataframe name here
   __ ~ __,            #dependent variable ~ factor
@@ -11,5 +11,9 @@ rstatix::cohens_d(
   hedges.correction = FALSE,
   ci = TRUE,
   conf.level = 0.95,
-  ci.type = "perc",
-  nboot = 1000)
+  ci.type = "bca",
+  nboot = 5000)
+
+#With the default settings here
+#95% confidence interval is bias-corrected and accelerated
+#5000 bootstrap samples taken 
--- script_templates/inference/wilcoxon_effect.R
+++ script_templates/inference/wilcoxon_effect.R
@@ -1,8 +1,10 @@
-#the code below will calculate the Cohen's d effect size when variances are unequal
+#the code below will calculate the standardized effect size (r) when variances are unequal
 #you need the rstatix package loaded for this code to work
 
 
-#standardized effect size for normal data
+#standardized effect size for non-normal data
+#2 independent groups
+#this is often called the Mann-Whitney U-test
 rstatix::wilcox_effsize(
   data = ___,         #put the dataframe name here
   __ ~ __,            #dependent variable ~ factor
@@ -10,5 +12,9 @@ rstatix::wilcox_effsize(
   alternative = "two.sided",
   ci = TRUE,
   conf.level = 0.95,
-  ci.type = "perc",
-  nboot = 1000)
+  ci.type = "bca",
+  nboot = 5000)
+
+#With the default settings here
+#95% confidence interval is bias-corrected and accelerated
+#5000 bootstrap samples taken 
