48401d71763b92de9d9b68260ea19e606a166404
Marney Pratt <mcpratt@smith.edu>
2025-03-10 14:49:04 -0400

    removed the mean

--- script_templates/graphing/boxplot_dots.R
+++ script_templates/graphing/boxplot_dots.R
@@ -11,7 +11,6 @@ box.dot.plot <- ggplot(
                outlier.shape= NA) +  #remove the outliers when all the points are included
   geom_quasirandom(                  #adds quasirandom jittered points
     shape=21, size=1, alpha = 0.5, width=0.25) + #sets the size, shape, and transparency of the points
-  stat_summary(fun=mean, geom="point", shape=4, size=2, colour="black", stroke=2) + #adds mean with an X
   ylab("___") + #put formatted label for the axis here (include units as needed)
   xlab("___") + #put formatted label for the axis here (include units as needed)
   coord_cartesian(expand=TRUE) +
--- script_templates/graphing/boxplot_dots_sampleSize.R
+++ script_templates/graphing/boxplot_dots_sampleSize.R
@@ -19,7 +19,6 @@ box.dot.plot <- ___ |>   #put the data frame name here
   geom_boxplot(width=0.1, alpha=0.8, outlier.shape= NA) +  #remove the outliers when all the points are included
   geom_quasirandom(                              #adds quasirandom jittered points but colors them by fill
     shape=21, size=1, alpha = 0.5, width=0.25) + #sets the size, shape, and transparency of the points
-  stat_summary(fun=mean, geom="point", shape=4, size=2, colour="black", stroke=2) + #adds mean with an X
   ylab("___") + #put formatted label for the axis here (include units as needed)
   xlab("___") + #put formatted label for the axis here (include units as needed)
   coord_cartesian(expand=TRUE) +
